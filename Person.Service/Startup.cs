e456e2b6d54df9ca676339ae42b998f96374fe10
Wil Braun <frederickwilbraun@gmail.com>
2018-05-02 15:19:54 -0400

    Updated dependencies

--- Person.Service/Startup.cs
+++ Person.Service/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Library.Interfaces;
+using Person.Library.Interfaces;
 using Swashbuckle.AspNetCore.Swagger;
 using Person.Library.Models;
 
