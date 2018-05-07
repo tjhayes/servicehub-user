56c17fe8127dca6b2eb29ebe1b865f4b46d0a84c
fred belotte <fred@belotte.xyz>
2018-05-07 02:29:00 -0400

    add the project sample unit tests. [#157238233]

---
+++ src/ServiceHub.Person.Testing/Context/TestSuite.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace ServiceHub.Person.Testing.Context
+{
+  public class TestSuite
+  {
+    [Fact]
+    public void SampleTest()
+    {
+      Assert.True(true);
+    }
+  }
+}
