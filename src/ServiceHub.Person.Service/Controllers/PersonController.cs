f8f46abfcce8c84a2cfd28051e04fd80c30a3869
Wil Braun <frederickwilbraun@gmail.com>
2018-05-08 16:55:30 -0400

    Added Person Client controller and NLog support [#157252750]

--- src/ServiceHub.Person.Service/Controllers/PersonController.cs
+++ src/ServiceHub.Person.Service/Controllers/PersonController.cs
@@ -1,38 +1,128 @@
-using System.Threading.Tasks;
+﻿using ServiceHub.Person.Library.Abstracts;
+using ServiceHub.Person.Library.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using CM = ServiceHub.Person.Context.Models;
 
 namespace ServiceHub.Person.Service.Controllers
 {
-  [Route("api/[conroller]")]
-  public class PersonController : Controller
-  {
-    public async Task<IActionResult> Get()
+    /// <summary>
+    /// This Controller will extend AModelController abstract class, used for Person service.
+    /// </summary>
+    [Route("api/[controller]")]
+    public class PersonController : AModelController<CM.Person>
     {
-      return await Task.Run(() => Ok());
-    }
+        public PersonController(IRepository<CM.Person> repo) : base(repo) { }
 
-    [HttpGet("{id}")]
-    public async Task<IActionResult> Get(int id)
-    {
-      return await Task.Run(() => Ok());
-    }
+        /// <summary>
+        /// This will perform GET operation based on Email field.
+        /// </summary>
+        /// <param name="email">Email id as string</param>
+        /// <returns>A document from Database, if unsuccessfull, returns 404</returns>
+        [HttpGet("email/{email}")]
+        public async Task<IActionResult> GetByEmail(string email)
+        {
+            var result2 = await Repo.GetAll();
 
-    [HttpPost]
-    public async Task<IActionResult> Post([FromBody]object value)
-    {
-      return await Task.Run(() => Ok());
-    }
+            var result = result2.FirstOrDefault(p => p.Email == email);
 
-    [HttpPut("{id}")]
-    public async Task<IActionResult> Put(int id, [FromBody]object value)
-    {
-      return await Task.Run(() => Ok());
-    }
+            if (result is null)
+            {
+                return NotFound();
+            }
 
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> Delete(int id)
-    {
-      return await Task.Run(() => Ok());
+            return Ok(result);
+        }
+
+        [HttpGet("name/{name}")]
+        public async Task<IActionResult> GetByName(string name)
+        {
+            var result2 = await Repo.GetAll();
+
+            var result = result2.FirstOrDefault(p => p.Name == name);
+
+            if (result is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet("batchName/{batchName}")]
+        public async Task<IActionResult> GetByBatchName(string batchName)
+        {
+            var result2 = await Repo.GetAll();
+
+            var result = result2.Where(p => p.BatchName == batchName);
+
+            if (result is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet("phone/{phone}")]
+        public async Task<IActionResult> GetByPhone(string phone)
+        {
+            var result2 = await Repo.GetAll();
+
+            var result = result2.FirstOrDefault(p => p.Phone == phone);
+
+            if (result is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet("hasCar/{hasCar}")]
+        public async Task<IActionResult> GetByHasCar(bool hasCar)
+        {
+            var result2 = await Repo.GetAll();
+
+            var result = result2.Where(p => p.HasCar == hasCar);
+
+            if (result is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet("isMale/{isMale}")]
+        public async Task<IActionResult> GetByIsMale(bool isMale)
+        {
+            var result2 = await Repo.GetAll();
+
+            var result = result2.Where(p => p.IsMale == isMale);
+
+            if (result is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet("address/{address}")]
+        public async Task<IActionResult> GetByAddress(string address)
+        {
+            var result2 = await Repo.GetAll();
+
+            var result = result2.Where(p => p.Address == address);
+
+            if (result is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
     }
-  }
 }
