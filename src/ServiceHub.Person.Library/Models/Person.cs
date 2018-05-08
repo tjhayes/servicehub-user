66662c0d580c2a1a0e2802f7190cd5584e4b3c5b
Wil Braun <frederickwilbraun@gmail.com>
2018-05-08 16:21:27 -0400

    Added Person Library Model [#157252740]

---
+++ src/ServiceHub.Person.Library/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using ServiceHub.Person.Library.Abstracts;
+using System.ComponentModel.DataAnnotations;
+
+namespace ServiceHub.Person.Library.Models
+{
+    public class Person : AModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string BatchName { get; set; }
+        [Phone]
+        public string Phone { get; set; }
+        [Required]
+        public bool HasCar { get; set; }
+        [Required]
+        public bool IsMale { get; set; }
+        [Required]
+        public string Address { get; set; }
+    }
+}
