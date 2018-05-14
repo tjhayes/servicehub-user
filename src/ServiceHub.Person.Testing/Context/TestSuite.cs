457ff35ccf5cdfa8f7f687491e78a510a97bf64d
Wil Braun <frederickwilbraun@gmail.com>
2018-05-14 13:36:18 -0400

    Tests for context

--- src/ServiceHub.Person.Testing/Context/TestSuite.cs
+++ src/ServiceHub.Person.Testing/Context/TestSuite.cs
@@ -1,13 +1,38 @@
+using ServiceHub.Person.Library.Models;
 using Xunit;
+using CM = ServiceHub.Person.Context.Models;
+using LM = ServiceHub.Person.Library.Models;
 
 namespace ServiceHub.Person.Testing.Context
 {
   public class TestSuite
   {
     [Fact]
-    public void SampleTest()
+    public void PersonTest()
     {
-      Assert.True(true);
+            var expected = typeof(CM.Person);
+
+            var actual = new CM.Person();
+
+            Assert.True(expected == actual.GetType());
+
+    }
+        [Fact]
+        public void PersonRepositoryTest()
+        {
+            var expected = typeof(CM.PersonRepository);
+
+
+
+            var Options = new LM.Settings
+            {
+                ConnectionString = "",
+                Database = "",
+                CollectionName = "",
+                CacheExpirationMinutes = 0
+            };
+
+            var actual = new CM.PersonRepository(new);
     }
   }
 }
