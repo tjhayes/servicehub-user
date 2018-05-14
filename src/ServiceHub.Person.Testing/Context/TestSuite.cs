586de79eab9ef9aa2213e7c58639ed227c32c96e
Wil Braun <frederickwilbraun@gmail.com>
2018-05-14 15:34:30 -0400

    Finished Person Context basic tests

--- src/ServiceHub.Person.Testing/Context/TestSuite.cs
+++ src/ServiceHub.Person.Testing/Context/TestSuite.cs
@@ -2,10 +2,13 @@ using ServiceHub.Person.Library.Models;
 using Xunit;
 using CM = ServiceHub.Person.Context.Models;
 using LM = ServiceHub.Person.Library.Models;
+using Moq;
+using Microsoft.Extensions.Options;
+
 
 namespace ServiceHub.Person.Testing.Context
 {
-  public class TestSuite
+    public class TestSuite
   {
     [Fact]
     public void PersonTest()
@@ -24,15 +27,18 @@ namespace ServiceHub.Person.Testing.Context
 
 
 
-            var Options = new LM.Settings
+            var Preferences = new Settings
             {
-                ConnectionString = "",
-                Database = "",
-                CollectionName = "",
+                ConnectionString = "mongodb://servicehub-dev:u2mZcmB3PS3x3hGStIbkQWJuty9fIlkQdNIIIwTbAtqvEofS6zanW7GGjLn6SZZdM8Hvi96SLGoaJXjhgCr71w==@servicehub-dev.documents.azure.com:10255/?ssl=true&replicaSet=globaldb",
+                Database = "test",
+                CollectionName = "test",
                 CacheExpirationMinutes = 0
             };
+            
 
-            var actual = new CM.PersonRepository(new);
+            var actual = new CM.PersonRepository(Options.Create<LM.Settings>(Preferences));
+
+            Assert.True(expected == actual.GetType());
     }
   }
 }
