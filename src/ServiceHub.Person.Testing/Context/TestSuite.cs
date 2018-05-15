5a6d618bfac08ae2825902b2ec13f78bab5694fb
Anthony Stephens <stephens.anthonyd@gmail.com>
2018-05-15 11:40:10 -0400

    Revert "jlkdfslkjfsad"
    This reverts commit a0d3bca66031cc03d59c0bd33a54a3fa69de919d.

--- src/ServiceHub.Person.Testing/Context/TestSuite.cs
+++ src/ServiceHub.Person.Testing/Context/TestSuite.cs
@@ -23,9 +23,20 @@ namespace ServiceHub.Person.Testing.Context
         [Fact]
         public void PersonRepositoryTest()
         {
-            var expected = typeof(CM.PersonRepository);            
+            var expected = typeof(CM.PersonRepository);
 
-            var actual = new CM.PersonRepository();
+
+
+            var Preferences = new Settings
+            {
+                ConnectionString = "mongodb://servicehub-dev:u2mZcmB3PS3x3hGStIbkQWJuty9fIlkQdNIIIwTbAtqvEofS6zanW7GGjLn6SZZdM8Hvi96SLGoaJXjhgCr71w==@servicehub-dev.documents.azure.com:10255/?ssl=true&replicaSet=globaldb",
+                Database = "test",
+                CollectionName = "test",
+                CacheExpirationMinutes = 0
+            };
+            
+
+            var actual = new CM.PersonRepository(Options.Create<LM.Settings>(Preferences));
 
             Assert.True(expected == actual.GetType());
     }
