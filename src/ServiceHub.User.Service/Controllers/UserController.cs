92a41b76a90844fb7a66765ff05028ca3c6605c9
fred belotte <fred@belotte.xyz>
2018-06-07 00:01:57 -0400

    add support for message queue to contollers. [#158170904]

--- src/ServiceHub.User.Service/Controllers/UserController.cs
+++ src/ServiceHub.User.Service/Controllers/UserController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.ServiceBus;
 using Microsoft.Extensions.Logging;
 
 namespace ServiceHub.User.Service.Controllers
@@ -7,7 +9,8 @@ namespace ServiceHub.User.Service.Controllers
   [Route("api/[controller]")]
   public class UserController : BaseController
   {
-    public UserController(ILoggerFactory loggerFactory) : base(loggerFactory) {}
+    public UserController(ILoggerFactory loggerFactory, IQueueClient queueClientSingleton)
+      : base(loggerFactory, queueClientSingleton) {}
     
     public async Task<IActionResult> Get()
     {
@@ -36,6 +39,23 @@ namespace ServiceHub.User.Service.Controllers
     public async Task<IActionResult> Delete(int id)
     {
       return await Task.Run(() => Ok());
+    }
+
+    protected override void UseReceiver()
+    {
+      var messageHandlerOptions = new MessageHandlerOptions(ReceiverExceptionHandler)
+      {
+        AutoComplete = false
+      };
+
+      queueClient.RegisterMessageHandler(ReceiverMessageProcessAsync, messageHandlerOptions);
+    }
+    
+    protected override void UseSender(Message message)
+    {
+      Task.Run(() =>
+        SenderMessageProcessAsync(message)
+      );
     }
   }
 }
