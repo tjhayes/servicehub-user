dffd90e7eae2bf05c6c91bb28939b415530ff17b
Timothy Hayes <tjhayes011@gmail.com>
2018-06-08 13:55:11 -0400

    add Name model

---
+++ src/ServiceHub.User.Library/Models/Name.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServiceHub.User.Library.Models
+{
+    public class Name
+    {
+        public Guid NameId { get; set; }
+        public string First { get; set; }
+        public string Middle { get; set; }
+        public string Last { get; set; }
+    }
+}
