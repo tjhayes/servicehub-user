2d3a2b007d5d9e216b981a2b018412ba44626354
Timothy Hayes <tjhayes011@gmail.com>
2018-06-09 10:20:57 -0400

    add more comments to User model

--- src/ServiceHub.User.Library/Models/User.cs
+++ src/ServiceHub.User.Library/Models/User.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace ServiceHub.User.Library.Models
 {
@@ -9,13 +7,17 @@ namespace ServiceHub.User.Library.Models
     /// </summary>
     public class User
     {
+        /// <value>The user's unique Id</value>
         public Guid UserId { get; set; }
         /// <value>The name of the training location</value>
         public string Location { get; set; }
         /// <value>The user's residential address</value>
         public Address Address { get; set; }
+        /// <value>The user's email address</value>
         public string Email { get; set; }
+        /// <value>Object storing the user's names (first, middle, last)</value>
         public Name Name { get; set; }
+        /// <value>The user's gender (Male/Female)</value>
         public string Gender { get; set; }
         /// <value>The user's job title</value>
         public string Type { get; set; }
