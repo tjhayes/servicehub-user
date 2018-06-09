9de85bcb47843dafaae048173624e15ce3ff5045
Austin A <austin.ascura@gmail.com>
2018-06-09 17:24:34 -0400

    add comments to Name model [#158036305]

--- src/ServiceHub.User.Library/Models/Name.cs
+++ src/ServiceHub.User.Library/Models/Name.cs
@@ -2,6 +2,13 @@
 
 namespace ServiceHub.User.Library.Models
 {
+    /// <summary>
+    /// A model for storing people's names.
+    /// </summary>
+    /// <remarks>
+    /// For people with multiple first/middle/last names
+    /// you can separate the names with whitespace/commas/etc.
+    /// </remarks>
     public class Name
     {
         public Guid NameId { get; set; }
