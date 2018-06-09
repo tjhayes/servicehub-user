ee77da925dea4519137a730e5582ee92f626014d
Austin A <austin.ascura@gmail.com>
2018-06-09 19:51:32 -0400

    updated Name model with constants and added validation [#158036305]

--- src/ServiceHub.User.Library/Models/Name.cs
+++ src/ServiceHub.User.Library/Models/Name.cs
@@ -19,5 +19,33 @@ namespace ServiceHub.User.Library.Models
         public string Middle { get; set; }
         /// <value>The user's surname(s) or last name(s)</value>
         public string Last { get; set; }
-    }
+
+        /// <value>The maximum length of the user's first name(s)</value>
+        private const int FIRST_MAX_LENGTH = 255;
+        /// <value>The maximum length of the user's middle name(s)</value>
+        private const int MIDDLE_MAX_LENGTH = 255;
+        /// <value>The maximum length of the user's last name(s)</value>
+        private const int LAST_MAX_LENGTH = 255;
+
+        /// <summary>
+        /// Check that the Name object represents a valid name.
+        /// </summary>
+        /// <remarks>
+        /// First and Last names are required, as well as Name Id. 
+        /// Middle name is optional.
+        /// No name fields can be an empty string.
+        /// </remarks>
+        /// <returns>True if the name is valid and false otherwise.</returns>
+        public Boolean Validate()
+        {
+            if (NameId == Guid.Empty) { return false; }
+            if (First == null || First == "") { return false; }
+            else if (First.Length > FIRST_MAX_LENGTH) { return false; }
+            if (Middle != null && 
+                (Middle == "" || Middle.Length > MIDDLE_MAX_LENGTH)) { return false; }
+            if (Last == null || Last == "") { return false; }
+            else if (Last.Length > LAST_MAX_LENGTH) { return false; }
+            
+            return true;
+        }
 }
