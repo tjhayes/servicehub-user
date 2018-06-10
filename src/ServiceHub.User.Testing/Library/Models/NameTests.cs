bbcc602cd4adef78bfb6ae851964befccb2ffa43
Austin A <austin.ascura@gmail.com>
2018-06-10 02:46:32 -0400

    adding tests for static validators for Name [#158036305]

--- src/ServiceHub.User.Testing/Library/Models/NameTests.cs
+++ src/ServiceHub.User.Testing/Library/Models/NameTests.cs
@@ -1,13 +1,122 @@
 ﻿using System;
 using Xunit;
+using ServiceHub.User.Library.Models;
 
 namespace ServiceHub.User.Testing.Library.Models
 {
-    class NameTests
+    public class NameTests
     {
-        private static string OversizedName = new string('A', 256);
-        private static string MaxLengthName = new string('A', 255);
-        private static string MinLengthName = new string('A', 1);
+        readonly string OversizedName = new string('A', 256);
+        readonly string MaxLengthName = new string('A', 255);
+        readonly string UndersizedName = new string('A', 0);
+        readonly string MinLengthName = new string('A', 1);
 
+        readonly Name ControlName = new Name() { NameId = Guid.NewGuid(), First = "John", Middle = "Jacob", Last = "Schmidt" };
+
+        #region ValidateFirst
+        [Fact]
+        public void ValidateFirst_NullArgument_ReturnsFalse()
+        {
+            // Arrange
+            // Act
+            var result = Name.ValidateFirst(null);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ValidateFirst_EmptyName_ReturnsFalse()
+        {
+            // Arrange
+            // Act
+            var result = Name.ValidateFirst(UndersizedName);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ValidateFirst_OversizedName_ReturnsFalse()
+        {
+            // Arrange
+            // Act
+            var result = Name.ValidateFirst(OversizedName);
+
+            // Assert
+            Assert.False(result);
+        }
+        #endregion
+
+        #region ValidateMiddle
+        [Fact]
+        public void ValidateMiddle_NullArgument_ReturnsFalse()
+        {
+            // Arrange
+            // Act
+            var result = Name.ValidateMiddle(null);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ValidateMiddle_EmptyName_ReturnsFalse()
+        {
+            // Arrange
+            // Act
+            var result = Name.ValidateMiddle(UndersizedName);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ValidateMiddle_OversizedName_ReturnsFalse()
+        {
+            // Arrange
+            // Act
+            var result = Name.ValidateMiddle(OversizedName);
+
+            // Assert
+            Assert.False(result);
+        }
+        #endregion
+
+        
+        #region ValidateLast
+        [Fact]
+        public void ValidateLast_NullArgument_ReturnsFalse()
+        {
+            // Arrange
+            // Act
+            var result = Name.ValidateLast(null);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ValidateLast_EmptyName_ReturnsFalse()
+        {
+            // Arrange
+            // Act
+            var result = Name.ValidateLast(UndersizedName);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ValidateLast_OversizedName_ReturnsFalse()
+        {
+            // Arrange
+            // Act
+            var result = Name.ValidateLast(OversizedName);
+
+            // Assert
+            Assert.False(result);
+        }
+        #endregion
     }
 }
