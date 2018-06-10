7948a091c2f418e5a5ccf2fef9c34ac86066a490
Austin A <austin.ascura@gmail.com>
2018-06-10 02:33:10 -0400

    adding test class for Name model [#158036305]

---
+++ src/ServiceHub.User.Testing/Library/Models/NameTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xunit;
+
+namespace ServiceHub.User.Testing.Library.Models
+{
+    class NameTests
+    {
+        private static string OversizedName = new string('A', 256);
+        private static string MaxLengthName = new string('A', 255);
+        private static string MinLengthName = new string('A', 1);
+
+    }
+}
