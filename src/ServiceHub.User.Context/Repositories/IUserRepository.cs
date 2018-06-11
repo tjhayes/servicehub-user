fc83a83bc36a469ac61e7d038267223d87e505c5
Timothy Hayes <tjhayes011@gmail.com>
2018-06-11 16:09:17 -0400

    add IUserRepository [#157252774]

---
+++ src/ServiceHub.User.Context/Repositories/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace ServiceHub.User.Context.Repositories
+{
+    /// <summary>
+    /// Repository interface for User model with CRUD functionality.
+    /// </summary>
+    public interface IUserRepository
+    {
+        void Insert(Library.Models.User user);
+        IQueryable<Library.Models.User> Get();
+        Library.Models.User GetById(Guid id);
+        void Update(Library.Models.User user);
+        void Delete(Guid id);
+        void DeleteAll();
+    }
+}
