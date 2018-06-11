7e86d7b3324fd5f4af475fb7a0b408f17f4d7fc4
Timothy Hayes <tjhayes011@gmail.com>
2018-06-11 18:21:08 -0400

    add .editorconfig [#158033226]

--- src/ServiceHub.User.Service/Controllers/UserController.cs
+++ src/ServiceHub.User.Service/Controllers/UserController.cs
@@ -7,61 +7,61 @@ using ServiceHub.User.Context.Repositories;
 
 namespace ServiceHub.User.Service.Controllers
 {
-  [Route("api/[controller]")]
-  public class UserController : BaseController
-  {
-    private readonly UserStorage _userStorage;
+    [Route("api/[controller]")]
+    public class UserController : BaseController
+    {
+        private readonly UserStorage _userStorage;
 
-    public UserController(ILoggerFactory loggerFactory, IQueueClient queueClientSingleton)
-      : base(loggerFactory, queueClientSingleton)
-    {
-      _userStorage = new UserStorage(new UserRepository());
-    }
-    
-    public async Task<IActionResult> Get()
-    {
-      return await Task.Run(() => Ok());
-    }
+        public UserController(ILoggerFactory loggerFactory, IQueueClient queueClientSingleton)
+          : base(loggerFactory, queueClientSingleton)
+        {
+            _userStorage = new UserStorage(new UserRepository());
+        }
 
-    [HttpGet("{id}")]
-    public async Task<IActionResult> Get(int id)
-    {
-      return await Task.Run(() => Ok());
-    }
+        public async Task<IActionResult> Get()
+        {
+            return await Task.Run(() => Ok());
+        }
 
-    [HttpPost]
-    public async Task<IActionResult> Post([FromBody]object value)
-    {
-      return await Task.Run(() => Ok());
-    }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return await Task.Run(() => Ok());
+        }
 
-    [HttpPut("{id}")]
-    public async Task<IActionResult> Put(int id, [FromBody]object value)
-    {
-      return await Task.Run(() => Ok());
-    }
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]object value)
+        {
+            return await Task.Run(() => Ok());
+        }
 
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> Delete(int id)
-    {
-      return await Task.Run(() => Ok());
-    }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody]object value)
+        {
+            return await Task.Run(() => Ok());
+        }
 
-    protected override void UseReceiver()
-    {
-      var messageHandlerOptions = new MessageHandlerOptions(ReceiverExceptionHandler)
-      {
-        AutoComplete = false
-      };
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return await Task.Run(() => Ok());
+        }
 
-      queueClient.RegisterMessageHandler(ReceiverMessageProcessAsync, messageHandlerOptions);
+        protected override void UseReceiver()
+        {
+            var messageHandlerOptions = new MessageHandlerOptions(ReceiverExceptionHandler)
+            {
+                AutoComplete = false
+            };
+
+            queueClient.RegisterMessageHandler(ReceiverMessageProcessAsync, messageHandlerOptions);
+        }
+
+        protected override void UseSender(Message message)
+        {
+            Task.Run(() =>
+              SenderMessageProcessAsync(message)
+            );
+        }
     }
-    
-    protected override void UseSender(Message message)
-    {
-      Task.Run(() =>
-        SenderMessageProcessAsync(message)
-      );
-    }
-  }
 }
