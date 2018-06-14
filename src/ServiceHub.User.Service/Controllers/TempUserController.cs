8edb268793c76c52b7e91e3f910649ad0687dfa0
Austin A <austin.ascura@gmail.com>
2018-06-14 10:40:40 -0400

    added GetByType and tests for UserController [#158033226]

--- src/ServiceHub.User.Service/Controllers/TempUserController.cs
+++ src/ServiceHub.User.Service/Controllers/TempUserController.cs
@@ -15,7 +15,7 @@ namespace ServiceHub.User.Service.Controllers
         public TempUserController(IUserRepository userRepository)
         {
             _userStorage = new UserStorage(userRepository);
-        }*/
+        }
 
         /// <summary>
         /// Get all users.
@@ -44,6 +44,40 @@ namespace ServiceHub.User.Service.Controllers
         public async Task<IActionResult> Get(Guid id)
         {
             return await Task.Run(() => Ok(_userStorage.GetById(id)));
+        }
+
+        /// <summary>
+        /// Gets all users of a certain type.
+        /// </summary>
+        /// <param name="type"> A string representing the type of user to filter by. </param>
+        /// <returns>If the type is not an accepted type, returns a 400 StatusCodeResult. If
+        /// the server fails to return a complete list of valid users, returns a 500
+        /// StatusCodeResut. Otherwise returns a list of validated Users. </returns>
+        [HttpGet]
+        [Route("type/{type}")]
+        [ProducesResponseType(500)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Library.Models.User>))]
+        public async Task<IActionResult> GetByType(/*[FromBody]*/ string type)
+        {
+            if(type == null) { return new StatusCodeResult(400); }
+            bool isValidType = false;
+            foreach (var validType in Library.Models.User.ValidUppercaseTypes)
+            {
+                if (type.ToUpper() == validType) { isValidType = true; }
+            }
+            if (!isValidType) { return new StatusCodeResult(400); }
+
+            try
+            {
+                var users = await Task.Run(() => _userStorage.Get());
+                var results = UserModelMapper.List_ContextToLibrary(users);
+                return Ok(results);
+            }
+            catch
+            {
+                return new StatusCodeResult(500);
+            }
         }
 
         [HttpPost]
--- src/ServiceHub.User.Testing/Service/UserControllerTests.cs
+++ src/ServiceHub.User.Testing/Service/UserControllerTests.cs
@@ -94,5 +94,57 @@ namespace ServiceHub.User.Testing.Service
 
             Assert.Equal(500, result.StatusCode);
         }
+
+        [Theory]
+        [InlineData("associate")]
+        [InlineData("ASSOCIATE")]
+        [InlineData("aSsOcIaTe")]
+        public async void GetByType_ValidTypes_Return200AndList(string type)
+        {
+            // Arrange
+            var mockRepo = new Mock<IUserRepository>();
+            // Act
+            mockRepo.Setup(x => x.Get()).Returns(contextUsers);
+
+            TempUserController controller = new TempUserController(mockRepo.Object);
+
+            var result = (ObjectResult) await controller.GetByType(type);
+
+            // Assert
+            Assert.Equal(200, result.StatusCode);
+            List<User.Library.Models.User> usersResult = (List<User.Library.Models.User>) result.Value;
+            var enumerator = usersResult.GetEnumerator();
+            enumerator.MoveNext();
+            Assert.Equal("John", enumerator.Current.Name.First);
+            enumerator.MoveNext();
+            Assert.Equal("Sophie", enumerator.Current.Name.First);
+            enumerator.Dispose();
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("notvalid")]
+        public async void GetByType_InvalidTypes_Return400(string type)
+        {
+            // Arrange
+            var mockRepo = new Mock<IUserRepository>();
+            // Act
+            mockRepo.Setup(x => x.Get()).Returns(contextUsers);
+
+            TempUserController controller = new TempUserController(mockRepo.Object);
+
+            var result = (StatusCodeResult) await controller.GetByType(type);
+
+            // Assert
+            Assert.Equal(400, result.StatusCode);
+            /*List<User.Library.Models.User> usersResult = (List<User.Library.Models.User>)result.Value;
+            var enumerator = usersResult.GetEnumerator();
+            enumerator.MoveNext();
+            Assert.Equal("John", enumerator.Current.Name.First);
+            enumerator.MoveNext();
+            Assert.Equal("Sophie", enumerator.Current.Name.First);
+            enumerator.Dispose();*/
+        }
     }
 }
